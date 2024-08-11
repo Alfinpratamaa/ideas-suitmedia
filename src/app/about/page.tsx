a65c232d889a285758a1418c76431027f0c45856
Muhamad Alfin Pratama <92765780+Alfinpratamaa@users.noreply.github.com>
2024-08-11 15:51:53 +0700

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,8 @@
 const AboutPage = () => {
     return (
-        <div>AboutPage</div>
+        <div className="flex items-center justify-center min-h-screen">
+            <h1 className="text-primary font-bold text-2xl">About Page</h1>
+        </div>
     )
 }
 
