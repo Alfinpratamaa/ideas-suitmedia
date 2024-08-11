0597a8214036a50b63a099118d23973a6604cbfa
muhamad alfin pratama <muhamadalfinpratamaa@gmail.com>
2024-08-11 15:35:21 +0700

    chore: Update Sanity configuration and dependencies

--- src/sanity/sanity.cli.ts
+++ src/sanity/sanity.cli.ts
@@ -3,6 +3,6 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: '91zpsdc5',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
 })
